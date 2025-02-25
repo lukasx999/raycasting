acf7cd53e908f4afd2ec298256d8b5f8138a631a
lukasx999 <116069013+lukasx999@users.noreply.github.com>
2025-02-25 21:24:06 +0100

    3d kinda working

--- src/main.rs
+++ src/main.rs
@@ -1,277 +1,18 @@
 use raylib::prelude::*;
 
+mod raycasting;
+use raycasting::{Player, Direction, Map, cast_rays};
 
-type CellType = i32;
-type Map = [[CellType; MAP_WIDTH]; MAP_HEIGHT];
-
-const SCREEN_WIDTH:  i32   = 1920;
-const SCREEN_HEIGHT: i32   = 1080;
-const MAP_WIDTH:     usize = 10;
-const MAP_HEIGHT:    usize = MAP_WIDTH;
-const CELL_SIZE:     i32   = 100;
-const PLAYER_STEP:   f32   = 0.3;
-
-const OFFSET: Vector2 = Vector2::new(
-    (SCREEN_WIDTH  / 2 - CELL_SIZE * MAP_WIDTH  as i32 / 2) as f32,
-    (SCREEN_HEIGHT / 2 - CELL_SIZE * MAP_HEIGHT as i32 / 2) as f32
-);
-
-//const OFFSET: Vector2 = Vector2::new(15.0, 15.0);
+const SCREEN_WIDTH:  i32 = 1920;
+const SCREEN_HEIGHT: i32 = 1080;
 
 
-fn map_connect_points(d: &mut RaylibDrawHandle, p1: Vector2, p2: Vector2, color: Color) {
-    let size = 5.0;
-    d.draw_line_ex(
-        p1 * CELL_SIZE as f32 + OFFSET,
-        p2 * CELL_SIZE as f32 + OFFSET,
-        size,
-        color
-    );
-}
 
-fn map_point(d: &mut RaylibDrawHandle, center: Vector2, size: f32, color: Color) {
-    d.draw_circle_v(
-        center * CELL_SIZE as f32 + OFFSET,
-        size,
-        color
-    );
-}
-
-fn map_square(d: &mut RaylibDrawHandle, pos: Vector2, color: Color) {
-    d.draw_rectangle_v(
-        pos * CELL_SIZE as f32 + OFFSET,
-        Vector2::new(CELL_SIZE as f32, CELL_SIZE as f32),
-        color
-    );
+trait UiComponent {
+    fn render(&self, draw: &mut RaylibDrawHandle);
 }
 
 
-fn get_cell_color(cell: i32) -> Option<Color> {
-    match cell {
-        1 => Some(Color::from_hex("585a5c").unwrap()),
-        2 => Some(Color::from_hex("164c82").unwrap()),
-        0 => None,
-        _ => panic!("Unknown cell type"),
-    }
-}
-
-
-#[derive(Debug, Clone, Copy)]
-enum Direction {
-    North,
-    East,
-    South,
-    West,
-}
-
-
-#[derive(Debug, Clone, Copy)]
-struct Player {
-    pub position:  Vector2,
-    pub direction: Vector2,
-    pub plane:     Vector2,
-}
-
-impl Player {
-    pub fn new() -> Self {
-        Self {
-            position:  Vector2::new(2.0, 5.0),
-            direction: Vector2::new(1.0, 0.0),
-            plane:     Vector2::new(0.0, 0.66),
-        }
-    }
-
-    pub fn move_(&mut self, dir: Direction) {
-        use Direction as D;
-        match dir {
-            D::North => self.position.y -= PLAYER_STEP,
-            D::East  => self.position.x += PLAYER_STEP,
-            D::South => self.position.y += PLAYER_STEP,
-            D::West  => self.position.x -= PLAYER_STEP,
-        }
-    }
-
-    // using_mouse will use smaller step
-    pub fn rotate(&mut self, counter_clockwise: bool, using_mouse: bool) {
-        let mut step = if counter_clockwise { -PLAYER_STEP } else { PLAYER_STEP };
-        if using_mouse {
-            step /= 3.0;
-        }
-        self.direction.rotate(step);
-        self.plane.rotate(step);
-    }
-
-    pub fn change_fov_len(&mut self, dec_else_inc: bool) {
-        let step = if dec_else_inc { -PLAYER_STEP } else { PLAYER_STEP };
-        self.direction.x += step;
-    }
-
-    pub fn change_fov_width(&mut self, dec_else_inc: bool) {
-        let step = if dec_else_inc { -PLAYER_STEP } else { PLAYER_STEP };
-        self.plane.y += step;
-    }
-
-    pub fn render(&self, draw: &mut RaylibDrawHandle) {
-        let color = Color::from_hex("3888c2").unwrap();
-
-        let pos    = self.position;
-        let dir    = self.position + self.direction;
-        let plane1 = dir + self.plane;
-        let plane2 = dir - self.plane;
-
-        map_connect_points(draw, dir, plane1, color);  // left
-        map_connect_points(draw, dir, plane2, color);  // right
-        map_connect_points(draw, pos, plane2, color);  // left-diagonal
-        map_connect_points(draw, pos, plane1, color);  // right-diagonal
-
-        map_point(draw, pos, 10.0, color);
-
-    }
-
-}
-
-
-fn render_map(draw: &mut RaylibDrawHandle, map: &Map) -> Result<(), std::num::ParseIntError> {
-    let color_cell_bg = Color::from_hex("2e2e2e")?;
-
-    for (y, row) in map.iter().enumerate() {
-        for (x, cell) in row.iter().enumerate() {
-            let color = get_cell_color(*cell).unwrap_or(color_cell_bg);
-
-            let rec = Rectangle::new(
-                x as f32 * CELL_SIZE as f32 + OFFSET.x,
-                y as f32 * CELL_SIZE as f32 + OFFSET.y,
-                CELL_SIZE as f32,
-                CELL_SIZE as f32,
-            );
-
-            draw.draw_rectangle_rec(rec, color);
-            draw.draw_rectangle_lines_ex(rec, 1.0, color_cell_bg.brightness(0.03));
-
-        }
-    }
-
-    Ok(())
-
-}
-
-
-// TODO: invert size
-fn render_stripe(
-    draw:  &mut RaylibDrawHandle,
-    x:     i32,
-    width: i32,
-    size:  f32,
-    color: Color
-) {
-    draw.draw_rectangle(
-        x,
-        ((SCREEN_HEIGHT as f32 * size) * (1.0 - size)) as i32,
-        width,
-        (SCREEN_HEIGHT as f32 * size) as i32,
-        color
-    );
-}
-
-fn cast_rays(draw: &mut RaylibDrawHandle, player: &Player, map: &Map) {
-
-    //for x in 0..=SCREEN_WIDTH {
-    let x = 0; { // only 1 ray for testing
-
-        /* -1.0 <-> 0.0 <-> 1.0 */
-        let camera_x = 2.0 * x as f32 / SCREEN_WIDTH as f32 - 1.0;
-
-        let ray_dir = player.direction + player.plane * camera_x;
-        let pos = player.position;
-
-        // the length of a step needed to get to the x/y edge of the next cell
-        // the formula is a simplified version of the pythagorean theorem
-        // => slope = ray_dir.y / ray_dir.x
-        // => sqrt(1 + slope.pow(2))
-        let delta_dist = Vector2::new(
-            (1.0 / ray_dir.x).abs(),
-            (1.0 / ray_dir.y).abs()
-        );
-
-        // the current cell of the map
-        // floating point value gets removed from player position
-        // has to be isize, because we later cast step to usize,
-        // and things will be messed up if step is negative
-        let (mut map_x, mut map_y) = (pos.x as isize, pos.y as isize);
-
-        // initial distance from player position to end of first cell
-        // will get incremented by delat_dist
-        let mut side_dist = Vector2::zero();
-
-        // step for incrementing map_x/y
-        let mut step = Vector2::zero();
-
-        // IMPORTANT: the x and y components of side_dist and delta_dist
-        // are both euclidean distances, not x/y coordinates
-
-        // TODO: why does multiplying perpendicular distance by delta_dist
-        // yield euclidean distance???
-        if ray_dir.x < 0.0 {
-            step.x = -1.0;
-            side_dist.x = (pos.x - map_x as f32) * delta_dist.x;
-        } else {
-            step.x = 1.0;
-            side_dist.x = (map_x as f32 + 1.0 - pos.x) * delta_dist.x;
-        }
-
-        if ray_dir.y < 0.0 {
-            step.y = -1.0;
-            side_dist.y = (pos.y - map_y as f32) * delta_dist.y;
-        } else {
-            step.y = 1.0;
-            side_dist.y = (map_y as f32 + 1.0 - pos.y) * delta_dist.y;
-        }
-
-        let point_size = 10.0;
-
-        // initial side dist
-        map_point(draw, pos + ray_dir * side_dist.x, point_size, Color::RED);
-        map_point(draw, pos + ray_dir * side_dist.y, point_size, Color::GREEN);
-
-
-        /* DDA */
-        loop {
-
-            // TODO: points are drawn before break
-
-            map_square(draw, Vector2::new(map_x as f32, map_y as f32), Color::GRAY.alpha(0.15));
-
-            if side_dist.x < side_dist.y {
-                side_dist.x += delta_dist.x;
-                map_x += step.x as isize;
-                map_point(draw, pos + ray_dir * side_dist.x, point_size, Color::RED);
-
-            } else {
-                side_dist.y += delta_dist.y;
-                map_y += step.y as isize;
-                map_point(draw, pos + ray_dir * side_dist.y, point_size, Color::GREEN);
-            }
-
-            // out of bounds check (no wall)
-            if map_x as usize >= MAP_WIDTH || map_y as usize >= MAP_HEIGHT {
-                break;
-            }
-
-            let cell = map[map_y as usize][map_x as usize];
-            let color = get_cell_color(cell);
-
-            if let Some(color) = color {
-                //render_stripe(d, x, CELL_SIZE, len, color);
-                map_square(draw, Vector2::new(map_x as f32, map_y as f32), Color::PURPLE);
-                break;
-            }
-
-        }
-
-
-    }
-
-}
 
 fn handle_keypress(key: KeyboardKey, player: &mut Player, show_map: &mut bool) {
     use KeyboardKey as K;
@@ -289,23 +30,6 @@ fn handle_keypress(key: KeyboardKey, player: &mut Player, show_map: &mut bool) {
     }
 }
 
-
-fn construct_map() -> Map {
-    [
-        [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ],
-        [ 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 ],
-        [ 1, 0, 0, 0, 0, 1, 2, 2, 0, 1 ],
-        [ 1, 0, 0, 0, 0, 0, 0, 1, 0, 0 ],
-        [ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
-        [ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
-        [ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
-        [ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
-        [ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
-        [ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
-    ]
-}
-
-
 fn init_raylib() -> (RaylibHandle, RaylibThread) {
 
     let (mut rl, thread) = raylib::init()
@@ -319,10 +43,9 @@ fn init_raylib() -> (RaylibHandle, RaylibThread) {
     (rl, thread) 
 }
 
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
 
-    let map = construct_map();
+    let map = Map::new();
     let mut player = Player::new();
     let mut show_map = true;
 
@@ -367,12 +90,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             handle_keypress(key, &mut player, &mut show_map);
         }
 
+        cast_rays(&mut draw, &player, &map);
+
         if show_map {
-            render_map(&mut draw, &map)?;
+            map.render(&mut draw);
             player.render(&mut draw);
         }
 
-        cast_rays(&mut draw, &player, &map);
 
     }
 
---
+++ src/raycasting.rs
@@ -0,0 +1,322 @@
+use raylib::prelude::*;
+
+use crate::{SCREEN_WIDTH, SCREEN_HEIGHT};
+
+
+
+
+const CELL_SIZE: i32 = 50;
+
+//const OFFSET: Vector2 = Vector2::new(
+//    (SCREEN_WIDTH  / 2 - CELL_SIZE * MAP_WIDTH  as i32 / 2) as f32,
+//    (SCREEN_HEIGHT / 2 - CELL_SIZE * MAP_HEIGHT as i32 / 2) as f32
+//);
+
+const OFFSET: Vector2 = Vector2::new(15.0, 15.0);
+
+
+
+
+
+type CellType = i32;
+const MAP_WIDTH: usize = 10;
+const MAP_HEIGHT: usize = MAP_WIDTH;
+
+pub struct Map([[CellType; MAP_WIDTH]; MAP_HEIGHT]);
+
+impl Map {
+    pub fn new() -> Self {
+        Self([
+            [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ],
+            [ 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 ],
+            [ 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 ],
+            [ 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 ],
+            [ 1, 0, 0, 0, 0, 0, 0, 2, 0, 1 ],
+            [ 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 ],
+            [ 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 ],
+            [ 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 ],
+            [ 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 ],
+            [ 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 ],
+        ])
+    }
+
+    pub fn get_cell(&self, x: usize, y: usize) -> CellType {
+        self.0[y][x]
+    }
+
+    pub fn render(&self, draw: &mut RaylibDrawHandle) {
+        let color_cell_bg = Color::from_hex("2e2e2e").unwrap();
+
+        for (y, row) in self.0.iter().enumerate() {
+            for (x, cell) in row.iter().enumerate() {
+                let color = get_cell_color(*cell).unwrap_or(color_cell_bg);
+
+                let rec = Rectangle::new(
+                    x as f32 * CELL_SIZE as f32 + OFFSET.x,
+                    y as f32 * CELL_SIZE as f32 + OFFSET.y,
+                    CELL_SIZE as f32,
+                    CELL_SIZE as f32,
+                );
+
+                draw.draw_rectangle_rec(rec, color);
+                draw.draw_rectangle_lines_ex(rec, 1.0, color_cell_bg.brightness(0.03));
+
+            }
+        }
+
+    }
+}
+
+
+// Helper functions
+
+fn map_connect_points(d: &mut RaylibDrawHandle, p1: Vector2, p2: Vector2, color: Color) {
+    let size = 5.0;
+    d.draw_line_ex(
+        p1 * CELL_SIZE as f32 + OFFSET,
+        p2 * CELL_SIZE as f32 + OFFSET,
+        size,
+        color
+    );
+}
+
+fn map_point(d: &mut RaylibDrawHandle, center: Vector2, size: f32, color: Color) {
+    d.draw_circle_v(
+        center * CELL_SIZE as f32 + OFFSET,
+        size,
+        color
+    );
+}
+
+fn map_square(d: &mut RaylibDrawHandle, pos: Vector2, color: Color) {
+    d.draw_rectangle_v(
+        pos * CELL_SIZE as f32 + OFFSET,
+        Vector2::new(CELL_SIZE as f32, CELL_SIZE as f32),
+        color
+    );
+}
+
+fn get_cell_color(cell: i32) -> Option<Color> {
+    match cell {
+        1 => Some(Color::from_hex("585a5c").unwrap()),
+        2 => Some(Color::from_hex("164c82").unwrap()),
+        0 => None,
+        _ => panic!("Unknown cell type"),
+    }
+}
+
+
+
+
+
+const PLAYER_STEP: f32 = 0.3;
+
+#[derive(Debug, Clone, Copy)]
+pub enum Direction {
+    North,
+    East,
+    South,
+    West,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct Player {
+    pub position:  Vector2,
+    pub direction: Vector2,
+    pub plane:     Vector2,
+}
+
+impl Player {
+    pub fn new() -> Self {
+        Self {
+            position:  Vector2::new(2.0, 5.0),
+            direction: Vector2::new(1.0, 0.0),
+            plane:     Vector2::new(0.0, 0.66),
+        }
+    }
+
+    pub fn move_(&mut self, dir: Direction) {
+        use Direction as D;
+        match dir {
+            D::North => self.position.y -= PLAYER_STEP,
+            D::East  => self.position.x += PLAYER_STEP,
+            D::South => self.position.y += PLAYER_STEP,
+            D::West  => self.position.x -= PLAYER_STEP,
+        }
+    }
+
+    pub fn rotate(&mut self, counter_clockwise: bool, using_mouse: bool) {
+        let mut step = if counter_clockwise { -PLAYER_STEP } else { PLAYER_STEP };
+        if using_mouse {
+            step /= 3.0;
+        }
+        self.direction.rotate(step);
+        self.plane.rotate(step);
+    }
+
+    pub fn change_fov_len(&mut self, dec_else_inc: bool) {
+        let step = if dec_else_inc { -PLAYER_STEP } else { PLAYER_STEP };
+        self.direction.x += step;
+    }
+
+    pub fn change_fov_width(&mut self, dec_else_inc: bool) {
+        let step = if dec_else_inc { -PLAYER_STEP } else { PLAYER_STEP };
+        self.plane.y += step;
+    }
+
+    pub fn render(&self, draw: &mut RaylibDrawHandle) {
+        let color = Color::from_hex("3888c2").unwrap();
+
+        let pos    = self.position;
+        let dir    = self.position + self.direction;
+        let plane1 = dir + self.plane;
+        let plane2 = dir - self.plane;
+
+        map_connect_points(draw, dir, plane1, color); // left
+        map_connect_points(draw, dir, plane2, color); // right
+        map_connect_points(draw, pos, plane2, color); // left-diagonal
+        map_connect_points(draw, pos, plane1, color); // right-diagonal
+
+        map_point(draw, pos, 10.0, color);
+
+    }
+
+}
+
+
+
+fn render_stripe(
+    draw:  &mut RaylibDrawHandle,
+    x:     i32,
+    width: i32,
+    mut size:  f32,
+    color: Color
+) {
+    size = 1.0 - size;
+    draw.draw_rectangle(
+        x,
+        ((SCREEN_HEIGHT as f32 * size) * (1.0 - size)) as i32,
+        width,
+        (SCREEN_HEIGHT as f32 * size) as i32,
+        color
+    );
+}
+
+pub fn cast_rays(draw: &mut RaylibDrawHandle, player: &Player, map: &Map) {
+
+    for x in 0..=SCREEN_WIDTH {
+    //for x in (0..=SCREEN_WIDTH).step_by(100) {
+    //let x = 0; { // only 1 ray for testing
+
+        /* -1.0 <-> 0.0 <-> 1.0 */
+        let camera_x = 2.0 * x as f32 / SCREEN_WIDTH as f32 - 1.0;
+
+        let ray_dir = player.direction + player.plane * camera_x;
+        let pos = player.position;
+
+        // the length of a step needed to get to the x/y edge of the next cell
+        // the formula is a simplified version of the pythagorean theorem
+        // => slope = ray_dir.y / ray_dir.x
+        // => sqrt(1 + slope.pow(2))
+        let delta_dist = Vector2::new(
+            (1.0 / ray_dir.x).abs(),
+            (1.0 / ray_dir.y).abs()
+        );
+
+        // the current cell of the map
+        // floating point value gets removed from player position
+        // has to be isize, because we later cast step to usize,
+        // and things will be messed up if step is negative
+        let (mut map_x, mut map_y) = (pos.x as isize, pos.y as isize);
+
+        // IMPORTANT: the x and y components of side_dist and delta_dist
+        // are both euclidean distances, not x/y coordinates
+
+        // step for incrementing map_x/y
+        let mut step = Vector2::zero();
+
+        // initial distance from player position to end of first cell
+        // will get incremented by delat_dist
+        let mut side_dist = Vector2::zero();
+
+
+        // TODO: why does multiplying perpendicular distance by delta_dist
+        // yield euclidean distance???
+        if ray_dir.x < 0.0 {
+            step.x = -1.0;
+            side_dist.x = (pos.x - map_x as f32) * delta_dist.x;
+        } else {
+            step.x = 1.0;
+            side_dist.x = (map_x as f32 + 1.0 - pos.x) * delta_dist.x;
+        }
+
+        if ray_dir.y < 0.0 {
+            step.y = -1.0;
+            side_dist.y = (pos.y - map_y as f32) * delta_dist.y;
+        } else {
+            step.y = 1.0;
+            side_dist.y = (map_y as f32 + 1.0 - pos.y) * delta_dist.y;
+        }
+
+        let mut side: i32;
+
+        // DDA
+        loop {
+
+            let color_ray = Color::GRAY;
+
+            if side_dist.x < side_dist.y {
+                //map_connect_points(draw, pos, pos + ray_dir * side_dist.x, color_ray);
+                side_dist.x += delta_dist.x;
+                map_x += step.x as isize;
+                side = 0;
+
+            } else {
+                //map_connect_points(draw, pos, pos + ray_dir * side_dist.y, color_ray);
+                side_dist.y += delta_dist.y;
+                map_y += step.y as isize;
+                side = 1;
+            }
+
+            // out of bounds check (no wall)
+            if map_x as usize >= MAP_WIDTH || map_y as usize >= MAP_HEIGHT {
+                break;
+            }
+
+            let cell = map.get_cell(map_x as usize, map_y as usize);
+            let color = get_cell_color(cell);
+
+            if let Some(mut color) = color {
+                //map_square(draw, Vector2::new(map_x as f32, map_y as f32), Color::PURPLE);
+
+                // substract delta_dist once, because the dda algorithm
+                // went one cell too far
+                let perp_wall_dist = if side == 0 {
+                    side_dist.x - delta_dist.x
+                } else {
+                    side_dist.y - delta_dist.y
+                };
+
+                let line  = (SCREEN_HEIGHT as f32 / perp_wall_dist) as i32;
+                let start = -line / 2 + SCREEN_HEIGHT / 2;
+                let end   =  line / 2 + SCREEN_HEIGHT / 2;
+
+                let len = perp_wall_dist / MAP_HEIGHT as f32;
+
+                // give x and y sides different brightness
+                if side == 1 {
+                    color = color.brightness(0.1);
+                }
+
+                //draw.draw_rectangle(x, start, CELL_SIZE, end, color);
+
+                render_stripe(draw, x, CELL_SIZE, len, color);
+                break;
+            }
+
+        }
+
+
+    }
+
+}
